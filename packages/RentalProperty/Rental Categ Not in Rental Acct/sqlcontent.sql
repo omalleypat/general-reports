1cbfd80fc9669ba1fe2f8c16c4eacbec1f41a522
omalleypat <omalleypat@users.noreply.github.com>
2023-07-31 06:30:27 -0500

    fix(category select)
    Was using parentid instead of categid for recursive query so top-level category would get missed.

--- packages/RentalProperty/Rental Categ Not in Rental Acct/sqlcontent.sql
+++ packages/RentalProperty/Rental Categ Not in Rental Acct/sqlcontent.sql
@@ -1,5 +1,5 @@
 WITH RECURSIVE rentalcategories(categid, categname, parentid) AS 
-(SELECT a.categid, a.categname, a.parentid FROM category_v1 a WHERE parentid = 26 
+(SELECT a.categid, a.categname, a.parentid FROM category_v1 a WHERE categid = 26 
 UNION ALL 
 SELECT c.categid, r.categname || ':' || c.categname, c.parentid 
 FROM rentalcategories r, category_v1 c 
