b8793d39bb60d6b8a724724873b7b1f3eb6ed2d2
omalleypat <omalleypat@users.noreply.github.com>
2023-07-31 06:27:36 -0500

    update RentalProperty categ all tx for db v18

--- packages/RentalProperty/Rental Category All Transactions/sqlcontent.sql
+++ packages/RentalProperty/Rental Category All Transactions/sqlcontent.sql
@@ -1,1 +1,11 @@
-SELECT ID, TransactionType, Date, Category, Subcategory, Amount, AccountName, ToAccountName, Splitted, Payee, TransactionNumber, Status, Notes FROM alldata WHERE CATEGID IS 26 ORDER BY Date
+WITH RECURSIVE rentalcategories(categid, categname, parentid) AS 
+(SELECT a.categid, a.categname, a.parentid FROM category_v1 a WHERE categid = 26 
+UNION ALL 
+SELECT c.categid, r.categname || ':' || c.categname, c.parentid 
+FROM rentalcategories r, category_v1 c 
+WHERE r.categid = c.parentid) 
+
+SELECT ID, TransactionType, Date, Category, Subcategory, Amount, AccountName, ToAccountName, Splitted, Payee, TransactionNumber, Status, Notes 
+FROM alldata 
+WHERE CATEGID in (SELECT categid FROM rentalcategories)
+ORDER BY Date
