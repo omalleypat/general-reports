bf8346ea7d99ff462c5d51f5f13fad4c60c90c87
omalleypat <omalleypat@users.noreply.github.com>
2023-07-30 20:25:03 -0500

    update "Rental categ not in rental acct" for db v18

--- packages/RentalProperty/Rental Categ Not in Rental Acct/sqlcontent.sql
+++ packages/RentalProperty/Rental Categ Not in Rental Acct/sqlcontent.sql
@@ -1,1 +1,8 @@
-SELECT * FROM alldata WHERE CATEGID IS 26 AND AccountID is not 17
+WITH RECURSIVE rentalcategories(categid, categname, parentid) AS 
+(SELECT a.categid, a.categname, a.parentid FROM category_v1 a WHERE parentid = 26 
+UNION ALL 
+SELECT c.categid, r.categname || ':' || c.categname, c.parentid 
+FROM rentalcategories r, category_v1 c 
+WHERE r.categid = c.parentid) 
+
+SELECT * FROM alldata WHERE CATEGID in (SELECT categid FROM rentalcategories) AND AccountID is not 17
