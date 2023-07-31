071518381b8af590d74e08d1f6388ff17e5e0c9e
omalleypat <omalleypat@users.noreply.github.com>
2023-07-31 06:56:53 -0500

    update report for rental categories to db v18

--- packages/RentalProperty/Non-Rental Category Transactions in Rental Account/sqlcontent.sql
+++ packages/RentalProperty/Non-Rental Category Transactions in Rental Account/sqlcontent.sql
@@ -1,1 +1,7 @@
-SELECT * FROM alldata WHERE ACCOUNTID is 17 AND CATEGID IS NOT 26 AND TransactionType IS NOT "Transfer"
+WITH RECURSIVE rentalcategories(categid, categname, parentid) AS 
+(SELECT a.categid, a.categname, a.parentid FROM category_v1 a WHERE categid = 26 
+UNION ALL 
+SELECT c.categid, r.categname || ':' || c.categname, c.parentid 
+FROM rentalcategories r, category_v1 c 
+WHERE r.categid = c.parentid) 
+SELECT * FROM alldata WHERE ACCOUNTID is 17 AND CATEGID NOT in (SELECT categid FROM rentalcategories) AND TransactionType IS NOT "Transfer"
